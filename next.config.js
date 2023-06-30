d643e7ebf79d9b19c8933b004e5b092701d3db2c
Haleem <ataulhaleem@gmail.com>
2023-06-30 19:28:03 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: 'export',
   experimental: {
     appDir: true,
-    output: 'export'
+    
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
